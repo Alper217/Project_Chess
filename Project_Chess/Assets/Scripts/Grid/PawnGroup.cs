d30581d960e265204ba624c9bf83b9d7cec5a17d
Alper_K <palikarya22@hotmail.com>
2026-03-31 13:56:31 +0300

    49_CardSystem_Update

--- Project_Chess/Assets/Scripts/Grid/PawnGroup.cs
+++ Project_Chess/Assets/Scripts/Grid/PawnGroup.cs
@@ -5,10 +5,10 @@ namespace AlperKocasalih.Chess.Grid
     public enum PawnGroup
     {
         None,
-        Group1,
-        Group2,
-        Group3,
-        Group4,
-        Group5
+        Melee,
+        Ranged,
+        Specialist,
+        Command,
+        Support
     }
 }
