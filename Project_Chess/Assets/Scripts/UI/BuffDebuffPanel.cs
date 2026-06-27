58cfa512530ce1239634bbd45452db7732606083
Alper_K <palikarya22@hotmail.com>
2026-06-27 16:19:59 +0300

    143_QuitButton

--- Project_Chess/Assets/Scripts/UI/BuffDebuffPanel.cs
+++ Project_Chess/Assets/Scripts/UI/BuffDebuffPanel.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 public class BuffDebuffPanel : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject exitPanel;
 
     private void Start()
     {
         panel.SetActive(false);
     }
-    
+
     public void OpenPanel()
     {
         bool isActive = panel.activeSelf;
         panel.SetActive(!isActive);
     }
-    
+
+    public void ReturnToMainMenu()
+    {
+        if (Unity.Netcode.NetworkManager.Singleton != null)
+        {
+            Unity.Netcode.NetworkManager.Singleton.Shutdown();
+        }
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+    public void OpenExitPanel()
+    {
+        exitPanel.SetActive(true);
+    }
+
+    public void CloseExitPanel()
+    {
+        exitPanel.SetActive(false);
+    }
 }
